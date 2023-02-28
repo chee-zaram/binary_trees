df9905d5fc3c35fb6b2819530f160e0b214b4123
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2023-02-28 10:25:12 +0100

    feat: add function to compute the size of a node in a binary tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Gets the size of a node in a binary tree
+ * @tree: Node for which size is to be gotten
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
