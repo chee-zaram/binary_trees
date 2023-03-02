1b20e0ec7229b3b7149c0484d70d6378b68117c1
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2023-03-02 23:35:25 +0100

    feat: add function to insert a new node into a bst

---
+++ 111-bst_insert.c
@@ -0,0 +1,56 @@
+#include "binary_trees.h"
+
+/**
+ * insert - Inserts a new node into the binary search tree
+ * @node: Pointer to the current node under evaluation
+ * @new_node: Pointer to the new node to insert
+ *
+ * Return: true if inserted correctly, false otherwise
+ */
+static bst_t *insert(bst_t *node, bst_t *new_node)
+{
+	if (new_node->n == node->n)
+		return (NULL);
+
+	if (!node->left && node->n > new_node->n)
+	{
+		node->left = new_node;
+		return (new_node);
+	}
+
+	if (!node->right && node->n < new_node->n)
+	{
+		node->right = new_node;
+		return (new_node);
+	}
+
+	return (new_node->n < node->n ? insert(node->left, new_node)
+				      : insert(node->right, new_node));
+}
+
+/**
+ * bst_insert - Inserts a node in a binary search tree
+ * @tree: Address of the pointer to the root node of the tree
+ * @value: Value stored in the node to be inserted
+ *
+ * Return: A pointer to the newly created node, or NULL otherwise
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *new_node = NULL;
+
+	if (!tree)
+		return (NULL);
+
+	new_node = binary_tree_node(NULL, value);
+	if (!new_node)
+		return (NULL);
+
+	if (!*tree)
+	{
+		*tree = new_node;
+		return (new_node);
+	}
+
+	return (insert(*tree, new_node));
+}
