bc2642ff45aad29a0d9cfa406590df484699d114
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2023-03-02 18:56:03 +0100

    feat: add function to perform left rotation on a binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -68,5 +68,6 @@ int            binary_tree_is_leaf(const binary_tree_t *node);
 void           _binary_tree_levelorder(const binary_tree_t *tree, size_t level,
 				       void (*func)(int));
 int            is_complete(const binary_tree_t *, size_t, size_t);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 #endif /* for _BINARY_TREES_ */
