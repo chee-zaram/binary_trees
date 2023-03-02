8d4bcdda02a374e779eccd05fa07dbeb0d857172
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2023-03-02 17:38:46 +0100

    feat: add function to check if a binary tree is complete

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,57 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: The binary tree to measure the size of.
+ *
+ * Return: The size of the tree.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * binary_tree_is_complete - Checks if a binary tree is complete.
+ * @tree: The binary tree to check.
+ *
+ * Return: If tree is NULL or not complete, return 0. Otherwise, return 1.
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	size_t          size, i, root_idx, lchild_idx, rchild_idx;
+	binary_tree_t **queue;
+
+	if (tree == NULL)
+		return (0);
+
+	size = binary_tree_size(tree);
+
+	queue = malloc(sizeof(binary_tree_t *) * size);
+	if (!queue)
+		return (0);
+
+	root_idx   = 0;
+	lchild_idx = (2 * i) + 1;
+	rchild_idx = (2 * i) + 2;
+
+	queue[root_idx] = (binary_tree_t *)tree;
+	for (i = 0; i < size; i++)
+	{
+		if (!queue[i])
+			break;
+
+		if (lchild_idx < size)
+		{
+			queue[lchild_idx] = queue[i]->left;
+			queue[rchild_idx] = queue[i]->right;
+		}
+	}
+
+	free(queue);
+
+	return (i == size);
+}
